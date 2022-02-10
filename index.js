ad3a8ce95dbbefe85a42fca62f330dff72d36361
deepak41 <ks.deepak07@gmail.com>
2022-02-10 20:20:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 7007;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json("Welcome to Node Server!")
+  res.json("Welcome to NodeJS Server!")
 });
 
 app.listen(PORT, () => {console.log(`The server is running at localhost:${PORT}`);});
